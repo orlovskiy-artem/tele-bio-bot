892bd09768d1f7da236f845c3796b17d7e02943c
orlovskiy-artem <artem.orlovsky@gmail.com>
2018-10-24 23:48:16 +0300

    Update run_server.py

--- run_server.py
+++ run_server.py
@@ -1,4 +1,4 @@
-import flask
+"""import flask
 from telebot import types
 from main import *
 from constants import *
@@ -16,3 +16,4 @@ def index():
 if __name__ == "__main__":
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
 
+"""
